11b7e164e1dc283c22c6809abc2547e37242274b
Josh Bradshaw <bradshaw@formlabs.com>
2019-05-22 15:18:53 -0400

    oh no

--- src/halimpl/pn54x/tml/i2c/phTmlNfc_i2c.c
+++ src/halimpl/pn54x/tml/i2c/phTmlNfc_i2c.c
@@ -454,7 +454,7 @@ int phTmlNfc_i2c_read(void *pDevHandle, uint8_t * pBuffer, int nNbBytesToRead)
         }
     }
     printf("ROUT-> ");
-    for (int i = 0: i < 260; i++;)
+    for (int i = 0; i < 260; i++;)
     {
         printf("0x%02X ", pBuffer[i]);
     }
@@ -480,7 +480,7 @@ int phTmlNfc_i2c_read(void *pDevHandle, uint8_t * pBuffer, int nNbBytesToRead)
 int phTmlNfc_i2c_write(void *pDevHandle, uint8_t * pBuffer, int nNbBytesToWrite)
 {
     printf("WOUT-> ");
-    for (int i = 0: i < nNbBytesToWrite; i++;)
+    for (int i = 0; i < nNbBytesToWrite; i++;)
     {
         printf("0x%02X ", pBuffer[i]);
     }
